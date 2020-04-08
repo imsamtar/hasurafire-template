b82be6c5b94cae4d2b677e0d4a9c568694766954
SAMEER TARIQ <imsamtar@gmail.com>
2020-04-08 19:16:36 +0500

    Added hasurafire

--- rollup.config.js
+++ rollup.config.js
@@ -33,7 +33,8 @@ export default {
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: ['svelte']
+			dedupe: ['svelte'],
+			mainFields: ['module', 'browser', 'main']
 		}),
 		commonjs(),
 
